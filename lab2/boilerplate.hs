6a7895fc49fd0fe363315c0daf171adead46a695
Andrew Laptev <a.d.laptev@gmail.com>
2015-12-03 10:01:59 +0300

    Update boilerplate.hs

--- lab2/boilerplate.hs
+++ lab2/boilerplate.hs
@@ -1,21 +1,32 @@
-a = "{\"abc\":true,\"def\":123,\"ghj\":{\"s\":61},\"ikl\":[1,2,3, \"123\"]}"
-
-data Token = TOpenBrace 
-  | TCloseBrace
-  | TOpenBracket 
-  | TCloseBracket
-  | TColon 
-  | TComma
-  | TString String
-  | TNumber Integer
-  | TBoolean Bool
-  | TNull deriving (Show)
-
 tokenize :: String -> [Token]
 tokenize string = 
   let
-    tokenize' [] tokens = tokens
-    tokenize' ('n':'u':'l':'l':xs) tokens = tokenize' xs (TNull:tokens)
-  in tokenize' string []
+    parseString acc ('\'':'"':xs) = parseString (acc ++ "\"") xs
+    -- \n, \t, ...
+    parseString acc ('"':xs) = (acc, xs)
+    parseString acc (x:xs) = parseString (acc ++ [x]) xs
+    parseString acc [] = error "!"--(acc, [])
 
-tokenize "null"
+    parseNumber acc (x:xs)
+      | x `elem` ['}',']',','] = (acc, (x:xs))
+      | otherwise = parseNumber (acc ++ [x]) xs
+    parseNumber acc [] = error "!"--(acc, [])
+
+    tokenize' [] tokens = tokens
+    tokenize' (' ':xs) tokens = tokenize' xs tokens
+    tokenize' ('n':'u':'l':'l':xs) tokens = tokenize' xs (TNull:tokens)
+    tokenize' ('t':'r':'u':'e':xs) tokens = tokenize' xs (TBoolean True:tokens)
+    tokenize' ('f':'a':'l':'s':'e':xs) tokens = tokenize' xs (TBoolean False:tokens)
+    tokenize' ('{':xs) tokens = tokenize' xs (TOpenBrace:tokens) 
+    tokenize' ('}':xs) tokens = tokenize' xs (TCloseBrace:tokens)
+    tokenize' ('[':xs) tokens = tokenize' xs (TOpenBracket:tokens)
+    tokenize' (']':xs) tokens = tokenize' xs (TCloseBracket:tokens)
+    tokenize' (':':xs) tokens = tokenize' xs (TColon:tokens)
+    tokenize' (',':xs) tokens = tokenize' xs (TComma:tokens)
+    tokenize' ('"':xs) tokens = 
+      let (s, xss) = parseString "" xs
+      in tokenize' xss ((TString s):tokens)
+    tokenize' xs tokens =
+      let (s, xss) = parseNumber "" xs
+      in tokenize' xss ((TNumber (read s :: Integer)):tokens)
+  in reverse $ tokenize' string []
